c956e12bc4669c7abd8151031080a02eb19f7de1
Cameron Moses <cameron.moses@fiig.com.au>
2021-09-16 09:41:16 +1000

    Change header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,24 +7,24 @@ class Header extends Component {
         return (
             <div className="App">
                 <nav className="navbar navbar-inverse navbar-fixed-top">
-                <div className="container">
-                    <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
-                    <a className="navbar-brand" href="/">Project name</a>
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="/">My React App</a>
+                        </div>
+                        <div id="navbar" className="collapse navbar-collapse">
+                            <ul className="nav navbar-nav">
+                                <li><Link to="/users" className="nav-item">Users</Link></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div id="navbar" className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/users" className="nav-item">Users</Link></li>
-                    </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+                </nav>
+            </div>
         )
     };
 }
